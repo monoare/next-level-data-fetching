f89f5240175c75ad8bbd7718cf4c291fd9d95c05
monoare <mamun2k9kuet@gmail.com>
2023-12-31 11:54:30 +0600

    added static site rendering and Incremental site generation

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,30 +1,34 @@
-import Link from "next/link";
-
 const PostsPage = async () => {
-  const res = await fetch("http://localhost:5000/posts", {
-    cache: "no-store",
+  // const res = await fetch("http://localhost:5000/posts");
+  // const posts = await res.json();
+
+  /* SSG--> Static Site Generation */
+  // const res = await fetch("http://localhost:5000/posts",{
+  //   cache: "force-cache" // force time a build time a statically create kore rakhe (by deafult)
+  // });
+
+  /* ISR--> Inremental Site Generation */
+  const res = await fetch("http://localhost:5000/posts",{
+    next: {
+      revalidate: 5,
+    }
   });
   const posts = await res.json();
-  //   console.log(posts);
+  // console.log(posts);
   return (
     <div className="w-full">
-      <h1 className="text-2xl">Total number of Posts:{posts.length}</h1>
+      <h1 className="text-2xl">Total Posts: {posts.length}</h1>
       {posts.map((post) => (
-        <div
-          key={post.id}
-          className="card bg-gray-100 shadow-xl w-[70%] mx-auto my-5"
-        >
-          <div className="card-body">
-            <h2 className="card-title">{post.title}</h2>
-            <p>{post.description}</p>
-            <p>Likes: {post.likesCount}</p>
-            <div className="card-actions justify-end">
-              <Link href={`/posts/${post.id}`}>
-                <button className="btn btn-primary">See More</button>
-              </Link>
-            </div>
+        <div key={post.id} className="card bg-gray-100 shadow-xl w-[70%] my-5 mx-auto">
+        <div className="card-body">
+          <h2 className="card-title">{post.title}</h2>
+          <p>{post.description}</p>
+          <p>{post.likesCount}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">See More</button>
           </div>
         </div>
+      </div>
       ))}
     </div>
   );
