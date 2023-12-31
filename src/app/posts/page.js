7f85a8164e7b53e6fddf49a794e1620957ce8f60
monoare <mamun2k9kuet@gmail.com>
2023-12-31 19:38:05 +0600

    added api related issue

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from "./Posts.module.css";
 
 const PostsPage = async () => {
   // const res = await fetch("http://localhost:5000/posts");
@@ -17,28 +18,31 @@ const PostsPage = async () => {
   // });
 
   /* SSR--> Server Site rendering */
-  const res = await fetch("http://localhost:5000/posts",{
-  cache: 'no-store'
+  const res = await fetch("http://localhost:5000/posts", {
+    cache: "no-store",
   });
 
   const posts = await res.json();
   // console.log(posts);
   return (
     <div className="w-full">
-      <h1 className="text-2xl">Total Posts: {posts.length}</h1>
+      <h1 className={styles.header_text}>Total Posts: {posts.length}</h1>
       {posts.map((post) => (
-        <div key={post.id} className="card bg-gray-100 shadow-xl w-[70%] my-5 mx-auto">
-        <div className="card-body">
-          <h2 className="card-title">{post.title}</h2>
-          <p>{post.description}</p>
-          <p>{post.likesCount}</p>
-          <div className="card-actions justify-end">
-            <Link href={`/posts/${post.id}`}>
-            <button className="btn btn-primary">See More</button>
-            </Link>
+        <div
+          key={post.id}
+          className="card bg-gray-100 shadow-xl w-[70%] my-5 mx-auto"
+        >
+          <div className="card-body">
+            <h2 className="card-title">{post.title}</h2>
+            <p>{post.description}</p>
+            <p>{post.likesCount}</p>
+            <div className="card-actions justify-end">
+              <Link href={`/posts/${post.id}`}>
+                <button className="btn btn-primary">See More</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
       ))}
     </div>
   );
